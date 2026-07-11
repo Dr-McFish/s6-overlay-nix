ffc0470c3349588e530105dc4799e54a42a61813
mcfish <noemail@example.com>
2026-07-11 10:00:33 +0200

    Modify s6-overlay-noarch to be more reproducable(hopefully)

--- s6-overlay-noarch.nix
+++ s6-overlay-noarch.nix
@@ -2,20 +2,30 @@
 
 { lib
 , stdenv
+, fetchFromGitHub 
 }:
 stdenv.mkDerivation rec {
   pname = "s6-overlay";
   version = import ./version.nix;
 
-  src = fetchTarball {
-    url = "https://github.com/just-containers/s6-overlay/releases/download/v${version}/s6-overlay-noarch.tar.xz";
-    sha256 = "066p7zb3gk3dmx7sj2mwdv82qc82bb3afj62v9ldk75gqv3h0v6p";
+  src = fetchFromGitHub {
+    owner = "just-containers";
+    repo = "s6-overlay";
+    rev = "v${version}";
+    hash = "sha256-JR7MiiFI1uJQthx8IU6vA6C0buqNJKlbu6D+gVgVXGM=";
   };
 
-  dontBuild = true;
+  buildPhase = ''
+    runHook preBuild
+    make rootfs-overlay-noarch-tarball
+    runHook postBuild
+  '';
 
   installPhase = ''
-    cp -r ${src} $out
+    runHook preInstall
+    mkdir -p $out
+    cp output/s6-overlay-noarch.tar.xz $out/
+    runHook postInstall
   '';
 
   meta = {
